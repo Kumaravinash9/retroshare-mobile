0083bb656f0b6f213233be3b514a7e0d0dcf5837
Kumaravinash9 <bhaiavinash9@gmail.com>
2021-09-03 23:38:36 +0530

    Added method channel

---
+++ android/app/src/main/kotlin/cc/retroshare/retroshare/Application.kt
@@ -0,0 +1,6 @@
+package cc.retroshare.retroshare
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugins.GeneratedPluginRegistrant
